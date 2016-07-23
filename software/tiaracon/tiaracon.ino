b1f919ae2d4b0f6cdb9aafa82d22e3fce956d016
Joe FitzPatrick <joefitz@securinghardware.com>
2016-07-23 01:24:06 -0700

    all todos completed, including: -fixed some spurious flashing due to poorly ordered charlieplexing commands -fixed slow flash mode to be interruptable -fixed sleep mode to not immediately wake up on button release -added a ton of comments

--- software/tiaracon/tiaracon.ino
+++ software/tiaracon/tiaracon.ino
@@ -1,25 +1,24 @@
-
+  
 /*
-Yet Another ATTiny-based Jar of Fireflies
+ATTiny code for 2016 Tiaracon Badge
 Joe FitzPatrick, @securelyfitz, joefitz@securinghardware.com
 
-Software, hardware, and build notes at https://git.io/fireflies
+Software, hardware, and build notes at https://git.io/tiaracon
 
-Arduino sketch for ATTiny45+ based jar of fireflies. 6 charlieplexed LEDs plus a capacitive button
-Based very heavily on Jason Webb's jar of fireflies:
+Arduino sketch for ATTiny45+ based tiaracon badge. 6 charlieplexed LEDs plus a button
+Derived from https://git.io/fireflies which in turn relied heavily on Jason Webb's jar of fireflies:
+Github repo: https://github.com/jasonwebb/Jar-of-Fireflies
 
 todos:
---be more consistent with globals vs. passed values
---clean up state change code
---fix 1 vs 2 led option
---switch power modes when in state 0 to prolong standby battery life https://learn.sparkfun.com/tutorials/h2ohno/low-power-attiny
---turn off ADC to save power
+DONE--be more consistent with globals vs. passed values
+DONE--clean up state change code
+DONE--fix 1 vs 2 led option
+DONE--switch power modes when in state 0 to prolong standby battery life https://learn.sparkfun.com/tutorials/h2ohno/low-power-attiny
+DONE--turn off ADC to save power
+DONE--tweak charlieplexing code to mininmize glitches and flashes
+--fix PWM code to minimize glitches and flashes.
 
 
-
-Jason Webb - zen.webb@gmail.com
-Project wiki: http://jason-webb.info/wiki/index.php?title=Jar_of_Fireflies
-Github repo: https://github.com/jasonwebb/Jar-of-Fireflies
 */
 #include <avr/sleep.h>
 #define F_CPU 1000000
@@ -34,6 +33,8 @@ const byte pinC = 2;
 int currmode=0;
 bool pressed= false;
 int currled=0;
+unsigned long startmillis=0;
+int elapsedmillis=0;
 
 void setup() {
   GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
@@ -42,24 +43,18 @@ void setup() {
   ADCSRA &= ~_BV(ADEN);                   // ADC off
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // everything off
 
-  pinMode(3,INPUT_PULLUP);
-/*  // flash each LED to show they work and so we know we just put in a good battery
-  do{
-    for(int j=0; j<6; j++) {
-      turnOn(j);
-//      delay(250);
-    }
-  }while (currmode=0);
-  randomSeed(millis());*/
+  pinMode(3,INPUT_PULLUP);  //pullup so it's normally high, active low button
 }
 
 ISR(PCINT0_vect)
 {
+  random(6);//adds entropy because its called for every event, even debounced ones
   // if we're not in the pressed state and you press the button, enter the pressed state and increment mode
   if (!digitalRead(3)&&!pressed){pressed=true;currmode++;}
   // if we're in the pressed state, and the button is now released, leave pressed state
   if (digitalRead(3)&&pressed){pressed=false;}
 }
+
 void loop() {  
   switch(currmode){
     case 0:    //all on
@@ -85,29 +80,36 @@ void loop() {
         analogOn(currled, brightness);
       }
       break;
-    case 4:   //random fade slow
-      randomSeed(millis());
-      currled=random(0,6);
-      for (int brightness=0;brightness<256;brightness++){
-        analogOn(currled, brightness);
-        delay(6);
-      }
-      for (int brightness=255;brightness>0;brightness--){
-        analogOn(currled, brightness);
-        delay(6);
+    case 4:   //random fade slow. Set a counter, and calculate LED PWM based on that counter
+      //if elapsed time exceeded - pick a led, start a counter
+      elapsedmillis=millis()-startmillis;   // calculate elapsed time so we know what to do
+      if (startmillis==0){     //this should only hit the very first time we get to this case
+        startmillis=millis();
+      }else if (elapsedmillis>=4600){   //timeout, choose a new LED and reset our start itme
+        startmillis+=elapsedmillis;
+        currled=random(6);  
+        analogOn(currled,0);
+      }else if (elapsedmillis>=4088){   //lights out time
+        turnOffAll();
+      }else if (elapsedmillis>=2048){   //fade out time
+        analogOn(currled,(-8-elapsedmillis)/8);      
+      }else{                            //fade in time 
+        analogOn(currled,(elapsedmillis)/8);
       }
       break;
-    case 5:
+    case 5:  // low power mode
       turnOffAll(); 
+      //wait until button is released before going to sleep, otherwise we'll just wake back up.
+      while(!digitalRead(3)){delay(100);}
       sleep_enable();
       sleep_cpu();
-    default:
+    default: // if we get here by mistake go back to zero.
       currmode=0;
       break;
   }
 }
 
-//useful test function
+//useful test function to output a value
 void flash(int count){
   for(int i=0;i<=count;i++){
     turnOn(1);
@@ -129,54 +131,74 @@ void turnOff(byte led) {
 
 //turns off all LEDs.
 void turnOffAll() {
+  //go to high impedence mode- disconnect all active power
   pinMode(pinA, INPUT);
   pinMode(pinB, INPUT);
   pinMode(pinC, INPUT);
+  //put everything low, so that no power is supplied when they go to output mode
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, LOW);
+  //clear all pwm setttings, analogWrite calls pinMode(output) before setting the output value!!
+  analogWrite(pinA, 0);
+  analogWrite(pinB, 0);
+  analogWrite(pinC, 0);
+  //by now, all pins should be 0v outputs without accidentally turning any on
 }
 // Write an analog value to an LED.
 void analogOn(byte led, byte value) {
+//  if (value<8){value=0;}
+//  if (value>240){value=255;}
   switch(led) {
     case 4:
       pinMode(pinC, INPUT);
+      
       digitalWrite(pinA, LOW);
       pinMode(pinA, OUTPUT);
+      
       analogWrite(pinB, value);
-      pinMode(pinB, OUTPUT);
       break;
     case 3:
       pinMode(pinC, INPUT);
+
       digitalWrite(pinB, LOW);
       pinMode(pinB, OUTPUT);
+      
       analogWrite(pinA, value);
-      pinMode(pinA, OUTPUT);
       break;
     case 5:
+      pinMode(pinC, INPUT); // to avoid PinB->PinC current
       pinMode(pinA, INPUT);
+      
       analogWrite(pinB, 255-value);
-      pinMode(pinB, OUTPUT);
+      
       digitalWrite(pinC, HIGH);
       pinMode(pinC, OUTPUT);
       break;
     case 2:
       pinMode(pinA, INPUT);
+      
       digitalWrite(pinC, LOW);
       pinMode(pinC, OUTPUT);
+
       analogWrite(pinB, value);
-      pinMode(pinB, OUTPUT);
       break;
     case 1:
+      pinMode(pinC, INPUT); // to avoid PinA->PinC current
       pinMode(pinB, INPUT);
+      
       analogWrite(pinA, 255-value);
-      pinMode(pinA, OUTPUT);
+
       digitalWrite(pinC, HIGH);
       pinMode(pinC, OUTPUT);
       break;
     case 0:
       pinMode(pinB, INPUT);
+      
       digitalWrite(pinC, LOW);
       pinMode(pinC, OUTPUT);
+
       analogWrite(pinA, value);
-      pinMode(pinA, OUTPUT);      
       break;
   }
 }
